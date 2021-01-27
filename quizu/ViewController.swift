9dc387f982703578c03d42c9816a382086b2aafd
tako124 <takovsika1@gmail.com>
2021-01-27 20:13:57 +0900

    Initial Commit

---
+++ quizu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizu
+//
+//  Created by Owner on 2021/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
